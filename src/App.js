092f15c0c84d13790fa568479d4bc3e307ea47fd
ruru <coco002197@gmail.com>
2022-12-22 12:22:35 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: ${(props) => {
+      return props.color ?? 'red'
+    }};
+`
+
+const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Wrapper>
+            <Title a="12">Hello World!</Title>
+            <Title color="green">Hello World!</Title>
+            <Title>Hello World!</Title>
+        </Wrapper>
+    )
 }
 
-export default App;
+export default App
